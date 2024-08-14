2da66e027fc2a7c0ea4de58f35f104b06102d4da
mik-305 <mik-305@bk.ru>
2024-08-14 10:28:49 +0300

    Add files via upload

---
+++ module_3_4.py
@@ -0,0 +1,14 @@
+def single_root_words(root_word, *other_words):
+    same_words = []  # Список слов, содержащих искомое(изначально пустой)
+    root_word = root_word.upper()  # Искомое слово в верхний регистр(для поиска)
+    spisok_param = other_words
+    for i in spisok_param:
+        if root_word in i.upper() or i.upper() in root_word:  # Если искомое слово есть(с учётом ИЛИ)
+            same_words.append(i)  #  заносим в список слово, содержащее искомое
+    print(same_words)
+
+print('---Обязательное слово в произв.переметрах---- ')
+single_root_words('дуб', 'Дубрава', 'ОгорОд', 'поДдубный', 'ЗадуБевший')
+print('---Слова из произв.переметров в обязательное слове ---- ')
+single_root_words('жеЛезныЙ', 'мЕдный', 'желЕ', 'кашА', 'железный АРГУМЕНТ')
+
